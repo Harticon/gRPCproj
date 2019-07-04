7d3cacef7e3668e76ac4b33127e5962b6eb8a681
Vojtech Hromadka <hromadkavojta@gmail.com>
2019-07-04 12:56:43 +0200

    feature(gRPC): user module ready, database added, http gateway added

--- user/cmd/user/main.go
+++ user/cmd/user/main.go
@@ -1,27 +1,96 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	order "github.com/Harticon/gRPCproj/user"
 	user "github.com/Harticon/gRPCproj/user/proto"
+	"github.com/golang/glog"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/spf13/viper"
 	"google.golang.org/grpc"
 	"log"
 	"net"
+	"net/http"
+	"time"
 )
+
+var (
+	// command-line options:
+	// gRPC server endpoint
+	grpcServerEndpoint1 = flag.String("/signup", "localhost:9090", "gRPC server endpoint")
+	grpcServerEndpoint2 = flag.String("/signin", "localhost:9090", "gRPC server endpoint")
+)
+
+func runHttpReverse() error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+
+	err := user.RegisterRouteGuideHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint1, opts)
+	if err != nil {
+		return err
+	}
+	err = user.RegisterRouteGuideHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint2, opts)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Reverse proxy is running")
+	return http.ListenAndServe(":8080", mux)
+
+}
+
+func runGrpcServer() error {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 9090))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+		return err
+	}
+	grpcServer := grpc.NewServer()
+
+	viper.SetDefault("connection", "grpc.db")
+	viper.SetDefault("secret", "secret")
+	viper.SetDefault("hashSecret", "salt&peper")
+
+	db, err := gorm.Open("sqlite3", viper.GetString("connection"))
+	if err != nil {
+		panic("failed to connect to database	")
+	}
+
+	db.AutoMigrate(&user.User{})
+
+	access := order.NewAccess(db)
+	service := order.NewRouteGuideServer(access)
+
+	user.RegisterRouteGuideServer(grpcServer, service)
+	fmt.Println("server is running")
+	err = grpcServer.Serve(lis)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 func main() {
 
 	flag.Parse()
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 8080))
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+
+	//todo ??? maybe error handling
+	go runGrpcServer()
+
+	time.Sleep(2 * time.Second)
+
+	defer glog.Flush()
+	if err := runHttpReverse(); err != nil {
+		glog.Fatal(err)
 	}
-	grpcServer := grpc.NewServer()
-	user.RegisterRouteGuideServer(grpcServer, &order.RouteGuideServer{})
-	fmt.Println("server is running")
-	err = grpcServer.Serve(lis)
-	if err != nil {
-		log.Fatalf("couldn't start server")
-	}
+
 }
--- user/service.go
+++ user/service.go
@@ -4,39 +4,45 @@ import (
 	"context"
 	"encoding/base64"
 	"errors"
+	"fmt"
 	user "github.com/Harticon/gRPCproj/user/proto"
 	"github.com/asaskevich/govalidator"
 	"github.com/spf13/viper"
 	"golang.org/x/crypto/scrypt"
-	"sync"
+	"google.golang.org/grpc/status"
+	"net/http"
 )
 
 type RouteGuideServer struct {
-	access     Accesser
-	mu         sync.Mutex
-	routeNotes *user.User
+	access Accesser
+}
+
+func NewRouteGuideServer(a Accesser) *RouteGuideServer {
+	return &RouteGuideServer{
+		access: a,
+	}
 }
 
 func (s *RouteGuideServer) SignUp(ctx context.Context, usr *user.User) (*user.User, error) {
 
 	if usr.Email == "" || usr.Password == "" {
-		return &user.User{}, errors.New("non valid user")
+		return &user.User{}, status.Error(http.StatusBadRequest, errors.New("not valid user").Error())
 	}
 
 	_, err := govalidator.ValidateStruct(usr)
 	if err != nil {
-		return &user.User{}, err
+		return &user.User{}, status.Error(http.StatusBadRequest, err.Error())
 	}
 
 	usr.Password, err = hashPassword(usr.Password)
 	if err != nil {
-		return &user.User{}, err
+		return &user.User{}, status.Error(http.StatusBadRequest, err.Error())
 	}
 
-	//_, err = s.access.CreateUser(*usr)
-	//if err != nil {
-	//	return &user.User{}, err
-	//}
+	_, err = s.access.CreateUser(*usr)
+	if err != nil {
+		return &user.User{}, err
+	}
 
 	return usr, nil
 }
@@ -47,21 +53,22 @@ func (s *RouteGuideServer) SignIn(ctx context.Context, usr *user.User) (*user.To
 
 	_, err := govalidator.ValidateStruct(usr)
 	if err != nil {
-		return &user.Token{}, err
+		return &user.Token{}, status.Error(http.StatusBadRequest, err.Error())
 	}
 
 	usr.Password, err = hashPassword(usr.Password)
 	if err != nil {
+		return &user.Token{}, status.Error(http.StatusBadRequest, err.Error())
+	}
+
+	query, err := s.access.GetUser(usr.Email, usr.Password)
+	if err != nil {
+		fmt.Println("error: couldnt find user", err)
 		return &user.Token{}, err
 	}
 
-	//_, err = s.access.GetUser(usr.Email, usr.Password)
-	//if err != nil {
-	//	fmt.Println("error: ", err)
-	//	return &user.Token{}, err
-	//}
-
-	return &user.Token{Token: "validtoken"}, err
+	fmt.Printf("user logged in %s with valid token\n", query.Email)
+	return &user.Token{Token: "validtoken"}, nil
 }
 
 func hashPassword(raw string) (string, error) {
